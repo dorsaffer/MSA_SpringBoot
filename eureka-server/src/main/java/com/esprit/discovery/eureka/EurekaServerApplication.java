d50fc4c5fb52a02fd755776311cab53ef6f14fac
dorsaf ferchichi <dorsaf.ferchichi@esprit.tn>
2020-11-27 22:55:53 +0100

    Add Eureka Server

---
+++ eureka-server/src/main/java/com/esprit/discovery/eureka/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.esprit.discovery.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+
+//l'annotation qui permet la découverte de services
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
