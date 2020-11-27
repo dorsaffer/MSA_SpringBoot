85ad2288fc446ac55010888a78ac38ba91153e26
dorsaf ferchichi <dorsaf.ferchichi@esprit.tn>
2020-11-27 22:54:52 +0100

    Add Condidat MS

---
+++ CandidatMS/src/main/java/com/esprit/microservice/CandidatMsApplication.java
@@ -0,0 +1,36 @@
+package com.esprit.microservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication 
+@EnableDiscoveryClient
+//@EnableEurekaClient
+public class CandidatMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidatMsApplication.class, args);
+	}
+	
+	@Autowired
+	private CandidatRepository repository;
+	
+	@Bean
+	ApplicationRunner init() {
+		return (args) -> {
+			// save
+			repository.save(new Candidat("Mariem", "Ch", "ma@esprit.tn"));
+			repository.save(new Candidat("Sarra", "ab", "sa@esprit.tn"));
+			repository.save(new Candidat("Mohamed", "ba", "mo@esprit.tn"));
+			// fetch
+			repository.findAll().forEach(System.out::println);
+
+		};
+	}
+
+}
